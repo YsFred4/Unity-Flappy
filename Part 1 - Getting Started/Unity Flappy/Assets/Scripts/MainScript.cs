c61831aaaeaf1ab1e3e3431d385615497b3627af
ysfred4 <steve.maier@gmail.com>
2014-04-03 17:59:32 -0400

    Code for the first post

---
+++ Part 1 - Getting Started/Unity Flappy/Assets/Scripts/MainScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScript : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject explosion;
+	private GameObject PlayerInstance;
+	private GameObject ExplodeInstance;
+
+	// Use this for initialization
+	void Start () {
+		CreateShip();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			DestroyShip();
+		}
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			CreateShip();
+		}
+	}
+
+	void CreateShip()
+	{
+		if(ExplodeInstance.gameObject != null)
+			DestroyObject(ExplodeInstance.gameObject);
+		PlayerInstance = (GameObject)Instantiate(player);
+	}
+
+	void DestroyShip()
+	{
+		ExplodeInstance = (GameObject)Instantiate(explosion, PlayerInstance.transform.position, Quaternion.identity);
+		DestroyObject(PlayerInstance.gameObject);
+	}
+
+}
