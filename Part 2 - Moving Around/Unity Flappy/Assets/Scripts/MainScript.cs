7caf61c4d90396a4185b254ec1e2595e3bbf0561
ysfred4 <steve.maier@gmail.com>
2014-06-03 10:18:12 -0400

    Add code for second article.

---
+++ Part 2 - Moving Around/Unity Flappy/Assets/Scripts/MainScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScript : MonoBehaviour {
+
+	public int speed;
+	public GameObject player;
+	public GameObject explosion;
+	private GameObject PlayerInstance;
+	private GameObject ExplodeInstance;
+
+	// Use this for initialization
+	void Start () {
+		CreateShip();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(Input.GetKeyDown(KeyCode.Escape))
+//		{
+//			DestroyShip();
+//		}
+//		if(Input.GetKeyDown(KeyCode.R))
+//		{
+//			CreateShip();
+//		}
+
+		{
+			ApplyForce();
+		}
+		if(Input.GetMouseButtonDown(0))
+		{
+			ApplyForce();
+		}		
+		if(Input.touchCount == 1)
+		{
+			Touch t = Input.GetTouch(0);
+			if(t.phase == TouchPhase.Began)
+			{
+				ApplyForce();
+			}
+		}
+	}
+
+	void ApplyForce()
+	{
+		if(PlayerInstance.gameObject != null)
+			PlayerInstance.rigidbody2D.AddForce(Vector2.up * speed);
+	}
+
+	void CreateShip()
+	{
+		if(ExplodeInstance.gameObject != null)
+			DestroyObject(ExplodeInstance.gameObject);
+		PlayerInstance = (GameObject)Instantiate(player);
+	}
+
+	void DestroyShip()
+	{
+		ExplodeInstance = (GameObject)Instantiate(explosion, PlayerInstance.transform.position, Quaternion.identity);
+		ExplodeInstance.rigidbody2D.velocity = PlayerInstance.rigidbody2D.velocity;
+		if(PlayerInstance.gameObject != null)
+			DestroyObject(PlayerInstance.gameObject);
+	}
+
+}
